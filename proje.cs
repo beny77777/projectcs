b4e1c1fe8da4a7ec1df6da837000b86cb77ccb08
beny77777 <redr93159@gmail.com>
2025-06-07 20:32:09 +0330

    class person

--- proje.cs
+++ proje.cs
@@ -1,4 +1,13 @@
-using System
+using System;
+using System.Collections.Generic;
+class Person
+{
+    public string FName;
+    public string LName;
+    public string Id;
+    public string Phone;
+    public string Address;
+}
 class Program
 {
     static void Main()
