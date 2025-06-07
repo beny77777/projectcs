ab87275f1ae087d805ae78c7c3fb3353ba038dd3
beny77777 <redr93159@gmail.com>
2025-06-08 00:48:48 +0330

    behtare ba prop benevisim

--- proje.cs
+++ proje.cs
@@ -2,12 +2,58 @@ using System;
 using System.Collections.Generic;
 class Person
 {
-    public string FName;
-    public string LName;
-    public string Id;
-    public string Phone;
-    public string Address;
+    public string FullName { get; set; } 
+    public string Id { get; set; } 
+    public string PhoneNumber { get; set; } 
+    public string Address { get; set; } /
 }
+
+public class Student : Person
+{
+    
+    private string _studentId;
+    private Room _Room;
+    private Block _residenceBlock;
+    private Dormitory _residenceDormitory;
+    private List<Equipment> _personalItems = new();
+
+   
+    public string StudentId
+    {
+        get { return _studentId; }
+        set { _studentId = value; }
+    }
+
+   
+    public Room Room
+    {
+        get { return _Room; }
+        set { _Room = value; }
+    }
+
+    
+    public Block ResidenceBlock
+    {
+        get { return _residenceBlock; }
+        set { _residenceBlock = value; }
+    }
+
+    
+    public Dormitory ResidenceDormitory
+    {
+        get { return _residenceDormitory; }
+        set { _residenceDormitory = value; }
+    }
+
+   
+    public List<Equipment> PersonalItems
+    {
+        get { return _personalItems; }
+        set { _personalItems = value ?? new List<Equipment>(); } 
+    }
+}
+
+
 class Program
 {
     static void Main()
