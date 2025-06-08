2a2eaa886d707248565f8b145ef52f8f763c3c3e
beny77777 <redr93159@gmail.com>
2025-06-08 18:47:39 +0330

    repair name

--- proje.cs
+++ proje.cs
@@ -5,30 +5,30 @@ class Person
     public string FullName { get; set; } 
     public string Id { get; set; } 
     public string PhoneNumber { get; set; } 
-    public string Address { get; set; } /
+    public string Address { get; set; } 
 }
 
 public class Student : Person
 {
-    
+   
     private string _studentId;
-    private Room _Room;
+    private Room _assignedRoom;
     private Block _residenceBlock;
     private Dormitory _residenceDormitory;
     private List<Equipment> _personalItems = new();
 
-   
+  
     public string StudentId
     {
         get { return _studentId; }
         set { _studentId = value; }
     }
 
-   
-    public Room Room
+    
+    public Room AssignedRoom
     {
-        get { return _Room; }
-        set { _Room = value; }
+        get { return _assignedRoom; }
+        set { _assignedRoom = value; }
     }
 
     
@@ -38,21 +38,20 @@ public class Student : Person
         set { _residenceBlock = value; }
     }
 
-    
+   
     public Dormitory ResidenceDormitory
     {
         get { return _residenceDormitory; }
         set { _residenceDormitory = value; }
     }
 
-   
     public List<Equipment> PersonalItems
     {
         get { return _personalItems; }
-        set { _personalItems = value ?? new List<Equipment>(); } 
+        set { _personalItems = value; }//حواسم هست که بعد برای برنامه اصلی . تست حواست به مقدار خالی باشد
+
     }
 }
-
 
 class Program
 {
@@ -60,5 +59,3 @@ class Program
     {
     }
 } 
-
-    
